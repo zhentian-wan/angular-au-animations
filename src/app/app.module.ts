adcd6ce83ba4d963397375df71dba5e693e6eff7
Zhentian Wan <answer881215@gmail.com>
2017-07-24 20:49:30 +0300

    init push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AuInputModule} from 'au-input';
+import {AuTabPanelModule} from 'au-tab-panel';
+import {AuMaskModule} from 'au-mask';
+import {AuModalModule} from 'au-modal';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    AuInputModule,
+    AuTabPanelModule,
+    AuMaskModule,
+    AuModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
