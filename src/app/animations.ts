db2dbcd8906e0bfc0c2a418edae45948c37fc923
Zhentian Wan <answer881215@gmail.com>
2017-07-26 21:53:51 +0300

    add different animations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,34 +1,80 @@
-import {animation, style, animate, trigger, transition, useAnimation} from '@angular/animations';
+import {animation, style, animate, trigger, transition, useAnimation, state} from '@angular/animations';
 
 /*
 * DSL
 * */
-export const fadeIn = animation([
+const fadeIn = animation([
   // start
   style({
-    opacity: 0,
-    'border-radius': '5px'
+    opacity: 0
   }),
   // end
   animate(
-    '1000ms',
+    "{{duration}} ease-in",
     style({
-      opacity: 1,
+      opacity: 1
     })
   )
 ]);
 
-export const fadeOut = animation(
+const fadeOut = animation(
   animate(
-    '500ms',
-    style({opacity: 0})
+    "{{duration}} ease-out",
+    style({
+      opacity: 0
+    })
   )
 );
+
+const heightStart =  animation([
+  style({
+    height: 0
+  }),
+  animate(
+    "{{duration}} ease-in",
+    style({
+      height: '*'
+    })
+  )
+]);
+
+const heightEnd = animation([
+  animate(
+    "{{duration}} ease-out",
+    style({
+      height: 0
+    })
+  )
+]);
+
 
 /*
 * Transition
 * */
 export const fadeInOut = trigger('fadeInOut', [
-  transition('void => *', useAnimation(fadeIn)),
-  transition('* => void', useAnimation(fadeOut))
+  transition(':enter', useAnimation(fadeIn, {params: {duration: '800ms'}})),
+  transition(':leave', useAnimation(fadeOut, {params: {duration: '200ms'}}))
 ]);
+
+// Using animation
+export const heightZeroFull = (params) => {
+  return trigger('heightZeroFull', [
+    transition(':enter', useAnimation(heightStart, {params})),
+    transition(':leave', useAnimation(heightEnd, {params}))
+  ]);
+};
+
+
+// Using state
+export const heightState = (params) => {
+  return trigger('heightState', [
+    state('inactive', style({
+      height: 0
+    })),
+    state('active',   style({
+      height: params.height
+    })),
+    transition('inactive => active', animate(`${params.duration} ease-in`)),
+    transition('active => inactive', animate(`${params.duration} ease-out`))
+  ]);
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import {fadeInOut} from './animations';
+import {fadeInOut, heightState, heightZeroFull} from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [fadeInOut]
+  animations: [
+    fadeInOut,
+    heightZeroFull({duration: '500ms'}),
+    heightState({height: '200px', duration: '500ms'})
+  ]
 })
 export class AppComponent {
   title = 'app';
+  state = 'active';
+
+  toggleState() {
+    if (this.state === 'inactive') {
+      this.state = 'active';
+    } else {
+      this.state = 'inactive';
+    }
+  }
 }
